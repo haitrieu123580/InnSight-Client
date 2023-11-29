fb49cf0649e96b61817d8481498119ecac1c14c0
yenle <yenle16032002@gmail.com>
2023-11-29 14:41:01 +0700

    callApi: add header to post new hotel

--- src/api/ApiHost.js
+++ src/api/ApiHost.js
@@ -4,11 +4,16 @@ const BASE_URL = `http://localhost:8001/api/hotel`
 const config = {
     headers: {
         'Content-Type': 'multipart/form-data',
-        // Accept: 'application/json'
+        // 'userId' : {userId}
     },
 };
 export const AddHotel = async (userID,newHotel) => {
-    const response = await axios.post(`${BASE_URL}/${userID}`, newHotel, config);
+    const response = await axios.post(`${BASE_URL}`, newHotel, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'userId' : `${userID}`
+        },
+    });
     console.log("res call",response)
     if (response.status === 201) {
         return { Data: true }
